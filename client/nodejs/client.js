51c5e8d8975c70777f79be46c6aa4ffa89dc7b2a
ludovicm67 <ludovicmuller1@gmail.com>
2018-02-28 21:21:57 +0100

    rewritten nodeJS client, added a prompt to ask user about server infos

--- client/nodejs/client.js
+++ client/nodejs/client.js
@@ -1,35 +1,63 @@
 // all what we need to import
-var net      = require('net');
-var readline = require('readline');
+const net      = require('net');
+const readline = require('readline');
+const prompts  = require('prompts');
 
-// client init
-var client = new net.Socket();
-client.setEncoding('utf-8');
+// informations we need to ask the user about the server location
+async function askServerDetails() {
+  let questions = [
+    {
+      type: 'text',
+      name: 'host',
+      message: 'Server host',
+      initial: 'localhost'
+    },
+    {
+      type: 'number',
+      name: 'port',
+      message: 'Server port',
+      initial: 4242,
+      min: 0,
+      max: 65535
+    }
+  ];
+  return await prompts(questions);
+};
 
-// try to connect
-client.connect(4242, '127.0.0.1', function() {
-  console.log('Connected!');
-  console.log('Type /quit to exit.\n\n')
-  client.write("Node client connected!\n");
-});
 
-// when we receive data, we log it in the console
-client.on('data', function(data) {
-  console.log(data.replace(/\n$/, ""));
-});
+// first we get all needed informations from the user, then we create the socket
+askServerDetails().then((response) => {
 
-// readline init
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+  // client socket init
+  const client = new net.Socket();
+  client.setEncoding('utf-8');
 
-// when we read a user input
-rl.on('line', function(data) {
-  if (data == '/quit') {
-    client.destroy();
-    rl.close();
-  } else {
-    client.write(data + "\n");
-  }
+  // try to connect
+  client.connect(response.port, response.host, () => {
+    console.log(`Connected to ${response.host}:${response.port}!`);
+    console.log('Type /quit to exit.\n\n')
+    client.write("Node client connected!\n");
+  });
+
+  // when we receive data, we log it in the console
+  client.on('data', data => {
+    console.log(data.replace(/\n$/, ""));
+  });
+
+  // readline init
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  // when we read a user input
+  rl.on('line', data => {
+    if (data == '/quit') {
+      client.destroy();
+      rl.close();
+      console.log("Goodbye!");
+    } else {
+      client.write(data + "\n");
+    }
+  });
 });
