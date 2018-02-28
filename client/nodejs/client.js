054fe2ebb08f9a8ab181aa04d71d7bcc47c2e735
ludovicm67 <ludovicmuller1@gmail.com>
2018-02-28 19:53:12 +0100

    updated node client: can now quit properly and added some comments

--- client/nodejs/client.js
+++ client/nodejs/client.js
@@ -1,26 +1,35 @@
-var net = require('net');
+// all what we need to import
+var net      = require('net');
 var readline = require('readline');
+
+// client init
 var client = new net.Socket();
 client.setEncoding('utf-8');
 
+// try to connect
 client.connect(4242, '127.0.0.1', function() {
   console.log('Connected!');
+  console.log('Type /quit to exit.\n\n')
   client.write("Node client connected!\n");
 });
 
-function send_data(data) {
-  client.write(data + "\n");
-}
-
+// when we receive data, we log it in the console
 client.on('data', function(data) {
   console.log(data.replace(/\n$/, ""));
 });
 
+// readline init
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// when we read a user input
 rl.on('line', function(data) {
-  send_data(data);
+  if (data == '/quit') {
+    client.destroy();
+    rl.close();
+  } else {
+    client.write(data + "\n");
+  }
 });
